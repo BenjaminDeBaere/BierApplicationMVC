50c33c1fc067f1936e530cac2dd7768b2811728c
BenjaminDeBaere <benjamin.de.baere1@gmail.com>
2017-02-15 15:57:55 +0100

    New commit

---
+++ MVCBierenApplicatie/Controllers/BierController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCBierenApplicatie.Models;
+
+namespace MVCBierenApplicatie.Controllers
+{
+    public class BierController : Controller
+    {
+        // GET: Bier
+        public ActionResult Index()
+        {
+            var bieren = new List<Bier>();
+            bieren.Add(new Bier { ID = 1, Naam = "Stella Artois", Alcohol = 15F });
+            bieren.Add(new Bier { ID = 2, Naam = "Heineken", Alcohol = 0.02F });
+            bieren.Add(new Bier { ID = 10, Naam = "Carlsberg", Alcohol = 5F });
+            return View("bieren",bieren);
+        }
+    }
+}
---
+++ MVCBierenApplicatie/Models/Bier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBierenApplicatie.Models
+{
+    public class Bier
+    {
+        [DisplayFormat(DataFormatString ="{0:000}")]
+        public Int32 ID { get; set; }
+        public string Naam { get; set; }
+        [UIHint("Kleuren")]
+        public float Alcohol { get; set; }
+    }
+}
